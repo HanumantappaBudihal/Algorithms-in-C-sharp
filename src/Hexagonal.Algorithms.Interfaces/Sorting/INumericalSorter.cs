b9c24908664c6fe14d3582c953a03c2ecac9c9d3
Hanumantappa Budihal <hanumanth.k.budihal@gmail.com>
2020-07-21 18:15:39 +0530

    Init setup for Bucker sort algorithm implementation

---
+++ src/Hexagonal.Algorithms.Interfaces/Sorting/INumericalSorter.cs
@@ -0,0 +1,11 @@
+﻿namespace Hexagonal.Algorithms.Interfaces.Sorting
+{
+    public interface INumericalSorter<T>
+    {
+        /// <summary>
+        /// TODO : Need to the code here
+        /// </summary>
+        /// <param name="inputArray"></param>
+        void Sort(T[] inputArray);
+    }
+}
---
+++ src/Hexagonal.Algorithms/Sorting/Numerical/BucketSort.cs
@@ -0,0 +1,24 @@
+﻿using Hexagonal.Algorithms.Interfaces.Sorting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hexagonal.Algorithms.Sorting.Numerical
+{
+    /// <summary>
+    /// TODO :
+    /// </summary>
+    public class BucketSort<T> : INumericalSorter<T> where T : IComparable<T>
+    {
+        //TODO : Need to think about the different approch for implementation
+        //TODO : How could we can implement the Bucker sort for all datatype, not only for Interger
+        //Imp cases : 
+        //1.Sort a large set of floating point numbers which are in range from 0.0 to 1.0 and are uniformly distributed across the range.
+        //How do we sort the numbers efficiently?
+        //2.Bucket Sort To Sort an Array with Negative Numbers
+        public void Sort(T[] inputArray)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
