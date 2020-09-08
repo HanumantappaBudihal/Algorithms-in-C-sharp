953daf0cf2d39f24ce25bc340b8aee20ba2515c0
Hanumantappa Budihal <hanumanth.k.budihal@gmail.com>
2020-09-08 22:51:56 +0530

    Tower of Hanio implementation in C#

---
+++ src/Hexagonal.Algorithms/Other/TowerOfHanoi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hexagonal.Algorithms.Other
+{
+    // Study References : 
+    //   from             auxilary         to
+    //    A=1                B=2           C=3
+    //    TowerOfHanio( n-1, 1, 3 ,2)
+    //    Move (1,3)
+    //    TowerOfHanio (n-1, 2, 1 ,3)
+    //  Approach : 
+    //  Shift (n-1) disks from A to B using C ( 132)
+    //  Shift last disk from A to C
+    //  Shitt (n-1) disks from B to C using A ( 213)
+    //Min move required : (2^n-1)
+    public class TowerOfHanoi
+    {
+        public static void PrintMoves(int numberOfDisks, string from, string auxiliary, string to)
+        {
+            if (numberOfDisks > 0)
+            {
+                PrintMoves(numberOfDisks - 1, from, to, auxiliary); //move (n-1) disk from A to B using C as auxiliary
+                Console.WriteLine("Move disk from " + from + " to " + to);//move last disk from A to C
+                PrintMoves(numberOfDisks - 1, auxiliary, from, to);// move n-1 from B to C using A as auxiliary 
+            }
+        }
+    }
+}
