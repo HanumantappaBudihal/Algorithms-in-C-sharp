4e7d3cf23157191986fd77f7aabf7f3750b226ed
Hanumantappa Budihal <hanumanth.k.budihal@gmail.com>
2020-09-08 23:37:52 +0530

    Finding the cube root : Using the binary seach approach : O(log n )

---
+++ src/Hexagonal.Algorithms/Knapsack/CubeRoot.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Hexagonal.Algorithms.Knapsack
+{
+    //Find the cube root of given x where x>0
+    class CubeRoot
+    {
+        // Find the cube root of the number using the Binary search approch
+        // Run time complexity : O(log n)
+        public static double GetCubeRoot(double number)
+        {
+            double low = 1.0;
+            double high = number;
+
+            while(Math.Abs(low-high)> 0.0000001)
+            {
+                double mid = (high + low) / 2.0;
+
+                if ((mid * mid * mid) > number)
+                    high = mid;
+                else
+                    low = mid;
+            }
+
+            return low;
+        }
+    }
+}
