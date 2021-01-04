3ca0b37711357077428fa1d20ffb186436b91d15
Hanumantappa Budihal <33819543+HanumantappaBudihal@users.noreply.github.com>
2021-01-04 23:00:46 +0530

    Update the ISearcher interface
    Update the comment ISearcher interface

--- src/Hexagonal.Algorithms.Interfaces/Search/ISearcher.cs
+++ src/Hexagonal.Algorithms.Interfaces/Search/ISearcher.cs
@@ -6,6 +6,7 @@
     /// <typeparam name="T"></typeparam>
     public interface ISearcher<T>
     {
+        //TODO : Need to implement the other functionalities
         int FindIndex(T[] inputArray, T itemToSeach);
     }
 }
